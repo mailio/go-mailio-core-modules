42815bf0637583e6252df48371005e586f4acf4f
igorrendulic <igor.amplio@gmail.com>
2022-07-06 10:03:19 -0600

    Added Server capabilities api: Igor Rendulic, MIR-7: Server Capabilities API [DRAFT], Mailio Improvement Proposals, no. 7, July 2022. [Online serial]. Available: https://mirs.mail.io/MIRS/mir-7

---
+++ api/api.go
@@ -0,0 +1,26 @@
+package api
+
+const (
+	// Possible protocols
+	PROTOCOL_HTTPS = "https"
+	PROTOCOL_PROTO = "proto"
+
+	PROTOCOL_PURPOSE_REST_API = "restapi"
+	PROTOCOL_PURPOSE_EXCHANGE = "exchange"
+)
+
+// ServerCapabilites
+type ServerCapabilities struct {
+	EmailDomains                     []string          `json:"emailDomains,omitempty"`                                     // supported email domains
+	SupportedProtocols               []*ServerProtocol `json:"supportedProtocols" validate:"required,min=2"`               // list of supported protocols (at least REST and Proto protocols)
+	MaximumMessageSizeBytes          int64             `json:"maximumMessageSizeBytes" validate:"required,min=1"`          // maximum message size in bytes
+	MaximumSingleAttachmentSizeBytes int64             `json:"maximumSingleAttachmentSizeBytes" validate:"required,min=1"` // maximum single attachment size in bytes
+}
+
+// Protocol description, purpose and version
+type ServerProtocol struct {
+	Name    string `json:"name,omitempty"`                                     // name of the protocol
+	Version string `json:"version" validate:"required"`                        // version of the protocol
+	Type    string `json:"type" validate:"required,oneof=https proto"`         // type of the protocol
+	Purpose string `json:"purpose" validate:"required,oneof=restapi exchange"` // purpose of the protocol
+}

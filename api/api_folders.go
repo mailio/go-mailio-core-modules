5816487295f86f7093208b98ff395834f40f9cc4
igorrendulic <igor.amplio@gmail.com>
2022-07-07 07:37:56 -0600

    added folder api structure

---
+++ api/api_folders.go
@@ -0,0 +1,8 @@
+package api
+
+type Folder struct {
+	ID             string `json:"id"`                       // id of the folder
+	Name           string `json:"name" validate:"required"` // name is required
+	TotalMessages  int64  `json:"totalMessages"`            // total number of messages in the folder
+	UnreadMessages int64  `json:"unreadMessages"`           // total number of unread messages in the folder
+}
